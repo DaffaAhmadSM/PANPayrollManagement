a2820418e4fa1cb9a7ccb854c5da8169c6c3d58c
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-05-27 09:42:29 +0700

    fix(database) table menu_users

--- database/migrations/2024_05_20_033303_create_menus_table.php
+++ database/migrations/2024_05_20_033303_create_menus_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('url');
+            $table->string('url')->nullable();
             $table->string('icon')->nullable();
             $table->unsignedBigInteger('parent_id')->nullable();
             $table->foreign('parent_id')->references('id')->on('menus');
--- database/seeders/UserMenuSeeder.php
+++ database/seeders/UserMenuSeeder.php
@@ -14,6 +14,77 @@ class UserMenuSeeder extends Seeder
      */
     public function run(): void
     {
+
+        $menu = [
+            [
+                "name" => "Setup",
+                "url" => null,
+                "order" => 1,
+                "level" => 0,
+            ],
+                [
+                    "name" => "User",
+                    "url" => "/user",
+                    "order" => 2,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Company",
+                    "url" => "/admin/setup/company",
+                    "order" => 3,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Number Sequence",
+                    "url" => "/admin/setup/sequence",
+                    "order" => 4,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Unit of Measure",
+                    "url" => "/admin/setup/unit-of-measure",
+                    "order" => 5,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "General Setup",
+                    "url" => "/admin/setup/general-setup",
+                    "order" => 6,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Multiplication Calculation",
+                    "url" => "/admin/setup/multiplication-calculation",
+                    "order" => 7,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Overtime Multiplication Setup",
+                    "url" => "/admin/setup/overtime-multiplication-setup",
+                    "order" => 8,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Working Hours",
+                    "url" => "/admin/setup/working-hours",
+                    "order" => 9,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Classification of Tax Payer",
+                    "url" => "/admin/setup/classification-of-tax-payer",
+                    "order" => 10,
+                    "level" => 0,
+                ],
+                [
+                    "name" => "Calendar Holiday",
+                    "url" => "/admin/setup/calendar-holiday",
+                    "order" => 11,
+                    "level" => 0,
+                ],
+            
+        ];
+
         $UserMenu = [
             [
                 'user_id' => 1,
@@ -35,38 +106,29 @@ class UserMenuSeeder extends Seeder
                 'user_id' => 1,
                 'menu_id' => 5,
             ],
-        ];
-
-        $menu = [
             [
-                "name" => "Dashboard",
-                "url" => "/dashboard",
-                "order" => 1,
-                "level" => 0,
+                'user_id' => 1,
+                'menu_id' => 6,
             ],
             [
-                "name" => "User",
-                "url" => "/user",
-                "order" => 2,
-                "level" => 0,
+                'user_id' => 1,
+                'menu_id' => 7,
             ],
             [
-                "name" => "Role",
-                "url" => "/role",
-                "order" => 3,
-                "level" => 0,
+                'user_id' => 1,
+                'menu_id' => 8,
             ],
             [
-                "name" => "Menu",
-                "url" => "/menu",
-                "order" => 4,
-                "level" => 0,
+                'user_id' => 1,
+                'menu_id' => 9,
             ],
             [
-                "name" => "Permission",
-                "url" => "/permission",
-                "order" => 5,
-                "level" => 0,
+                'user_id' => 1,
+                'menu_id' => 10,
+            ],
+            [
+                'user_id' => 1,
+                'menu_id' => 11,
             ],
         ];
 
@@ -77,11 +139,39 @@ class UserMenuSeeder extends Seeder
             ],
             [
                 'id' => 3,
-                'parent_id' => 2,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 4,
+                'parent_id' => 1,
             ],
             [
                 'id' => 5,
-                'parent_id' => 4,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 6,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 7,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 8,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 9,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 10,
+                'parent_id' => 1,
+            ],
+            [
+                'id' => 11,
+                'parent_id' => 1,
             ],
         ];
 
