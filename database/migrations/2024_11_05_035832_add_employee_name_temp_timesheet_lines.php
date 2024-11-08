8a60c212963889ac38efa489a7fa1af6e959a95a
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2024-11-08 15:30:39 +0700

    change overtimecalculation and database structure

---
+++ database/migrations/2024_11_05_035832_add_employee_name_temp_timesheet_lines.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('temp_timesheet_lines', function (Blueprint $table) {
+            $table->string('employee_name')->nullable();
+            $table->string('job_dissipline')->nullable();
+            $table->string('leg_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('temp_timesheet_lines', function (Blueprint $table) {
+            $table->dropColumn('employee_name');
+            $table->dropColumn('job_dissipline');
+            $table->dropColumn('leg_id');
+        });
+    }
+};
