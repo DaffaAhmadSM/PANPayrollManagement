037a88d5f2bff026221fc72d90451e1b4578490f
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2024-11-10 10:43:41 +0700

    add migration for real and customer timesheets

--- database/migrations/2024_11_10_025214_add_employeename_time_sheet_line.php
+++ database/migrations/2024_11_10_025214_add_employeename_time_sheet_line.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('temp_timesheet_lines', function (Blueprint $table) {
+        Schema::table('time_sheet_lines', function (Blueprint $table) {
             $table->string('employee_name')->nullable();
             $table->string('job_dissipline')->nullable();
             $table->string('leg_id')->nullable();
             $table->string('slo_no')->nullable();  
             $table->integer('rate')->default(1);
-            
         });
     }
 
@@ -26,7 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('temp_timesheet_lines', function (Blueprint $table) {
+        Schema::table('time_sheet_lines', function (Blueprint $table) {
             $table->dropColumn('employee_name');
             $table->dropColumn('job_dissipline');
             $table->dropColumn('leg_id');
