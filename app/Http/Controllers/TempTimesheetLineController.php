2ef50437a5d17c77c5b3f73e017e353bce86dffd
DaffaAhmadSM <Daffamubaroqi321@gmail.com>
2024-12-09 17:22:04 +0700

    refactor overtimecalculation flow

--- app/Http/Controllers/TempTimesheetLineController.php
+++ app/Http/Controllers/TempTimesheetLineController.php
@@ -49,11 +49,15 @@ class TempTimesheetLineController extends Controller
             tempTimeSheetOvertime::where("random_string", $temp_timesheet_str)->delete();
         }
 
-        $mcd = TempMcd::where('temp_time_sheet_id', $temptimesheet->id)->get();
+        $mcd = TempMcd::where('temp_time_sheet_id', $temptimesheet->id)->cursor();
+        // return $mcd;
+        // $mcd Group by employee name and date
+        $mcd = $mcd->groupBy(['employee_name', 'date'])->collapse();
+        // return $mcd;
         $customer = Customer::find($temptimesheet->customer_id);
         $working_hour_detail = WorkingHoursDetail::where('working_hours_id', $customer->working_hour_id)->get();
         $calendar_holiday = CalendarHoliday::whereBetween('date', [$temptimesheet->from_date, $temptimesheet->to_date])->get();
-        $overtime_multiplication_all = OvertimeMultiplicationSetup::all();
+        $overtime_multiplication_all = OvertimeMultiplicationSetup::with('calculation')->get();
         // $position_rate = PositionRate::all();
 
         try {
@@ -62,8 +66,8 @@ class TempTimesheetLineController extends Controller
         $timesheet_overtime = [];
         $create_position_rate = [];
         $count = 0;
-        foreach ($mcd as $item) {
-            $date = Carbon::parse($item->date);
+        foreach ($mcd as $datemcd){
+            $date = Carbon::parse($datemcd->first()->date);
             $day = $date->dayName;
             $working_day = $working_hour_detail->firstWhere("day", $day);
             $working_day_hours = $working_day ? $working_day->hours : 0;
@@ -72,130 +76,186 @@ class TempTimesheetLineController extends Controller
                 $is_holiday = true;
             }
             $holiday = $is_holiday ? true : false;
+            $total_actual_hour = 0;
+            $deduction_hour = 0;
             if(!$working_day || $holiday) {
                 $working_day_hours = 0;
-                $deduction_hour = 0;
-                $overtime_hour = $item->value;
-            }else{
-                $deduction_hour = $item->value < $working_day_hours ? $working_day_hours - $item->value : 0;
-                $overtime_hour = $item->value > $working_day_hours ? $item->value - $working_day_hours : 0;    
             }
-           $total_overtime_hours = 0;
-            
-            if ($overtime_hour > 0) {
-                if ($holiday) {
+
+            if ($holiday) {
+                $overtime_multiplication = $overtime_multiplication_all
+                ->where('day_type', 'Holiday')->where('day', $day)
+                ->all();
+                if (!$overtime_multiplication) {
                     $overtime_multiplication = $overtime_multiplication_all
-                    ->where('day_type', 'Holiday')->where('day', $day)
-                    ->where('from_hours', '<=', $overtime_hour)
-                    ->all();
-                    if (!$overtime_multiplication) {
-                        $overtime_multiplication = $overtime_multiplication_all
-                        ->where('day_type', 'Holiday')
-                        ->where('day', "all")
-                        ->where('from_hours', '<=', $overtime_hour)->all();
-                    }
-                }else{
+                    ->where('day_type', 'Holiday')
+                    ->where('day', "all")->all();
+                }
+            }else{
+                $overtime_multiplication = $overtime_multiplication_all
+                ->where('day_type', 'Normal')
+                ->where('day', $day)->all();
+                if (!$overtime_multiplication){
                     $overtime_multiplication = $overtime_multiplication_all
                     ->where('day_type', 'Normal')
-                    ->where('day', $day)
-                    ->where('from_hours', '<=', $overtime_hour)->all();
-                    if (!$overtime_multiplication){
-                        $overtime_multiplication = $overtime_multiplication_all
-                        ->where('day_type', 'Normal')
-                        ->where('day', 'all')
-                        ->where('from_hours', '<=', $overtime_hour)->all();
-                    }
-                }
-                $remaining = $overtime_hour;
-                if ($overtime_multiplication) {
-                    foreach ($overtime_multiplication as $multiplication) {
-                        $fromHour = $multiplication->from_hours;
-                        $toHour = $multiplication->to_hours;
-                        $to = $toHour - $fromHour;
-                        $multiplier = $multiplication->calculation->multiplier;
-
-                        if ($remaining <= 0){
-                            break;
-                        }
-                        $range = min($to, $remaining);
-                        $result = $range * $multiplier;
-                        $remaining -= $range;
-                        // return $range. ' - ' . $remaining. ' - ' . $result . '-'. $from . ' - ' . $to;
-                        $timesheet_overtime[] = [
-                            "random_string" => $temp_timesheet_str,
-                            "multiplication_id" => $multiplication->id,
-                            "multiplication_code" => $multiplication->calculation->code,
-                            "hours" => $range,
-                            "total_hours" => $result,
-                            "custom_id" => $temp_timesheet_str . '-' . $count
-                        ];
-
-                        $total_overtime_hours += $result;
-                    }
-                }else{
-                    $total_overtime_hours = $overtime_hour;
+                    ->where('day', 'all')->all();
                 }
             }
 
-            // $is_position_rate = $position_rate->firstWhere('position', $item->job_dissipline);
-            // if($item->rate > 1) {
-            //     // create new position rate
-            //     $create = [
-            //         'position' => $item->job_dissipline,
-            //         'from_date' => $temptimesheet->from_date,
-            //         'to_date' => $temptimesheet->to_date,
-            //         'type' => 'daily',
-            //         'rate' => $item->rate,
-            //         'meal_per_day' => 0
-            //     ];
+            $Xremains = collect([]);
+            foreach ($overtime_multiplication as $data){
+                $Xremains->push([
+                    'code' => $data->calculation->code,
+                    'remaining' => $data->to_hours - $data->from_hours,
+                ]);
+            }
 
-            //     $create_position_rate[] = $create;
-            //     $rate = $item->rate;
-            //     // add to position rate
-            //     $position_rate->push($create);
-            // }else{
-            //     if ($is_position_rate) {
-            //         $rate = $is_position_rate['rate'];
-            //     }else{
-            //         $create = [
-            //             'position' => $item->job_dissipline,
-            //             'from_date' => $temptimesheet->from_date,
-            //             'to_date' => $temptimesheet->to_date,
-            //             'type' => 'daily',
-            //             'rate' => $item->rate,
-            //             'meal_per_day' => 0
-            //         ];
+            foreach ($datemcd as $item) {
+                $deduction_hour = $item->value < $working_day_hours ? $working_day_hours - $item->value : 0;
+                if($working_day_hours < 0){
+                    $overtime_hour = $item->value;
+                }else{
+                    $overtime_hour = $item->value > $working_day_hours ? $item->value - $working_day_hours : 0;
+                }
+                $total_overtime_hours = 0;
+                $total_actual_hour += $item->value;
+                // return $total_actual_hour;
 
-            //         $create_position_rate[] = $create;
-            //         $rate = $item->rate;
-            //         // add to position rate
-            //         $position_rate->push($create);
-            //     }
-            // }
-            // return $timesheet_overtime;
+                
+                if ($overtime_hour > 0) {
+                    // if ($holiday) {
+                    //     $overtime_multiplication = $overtime_multiplication_all
+                    //     ->where('day_type', 'Holiday')->where('day', $day)
+                    //     // ->where('from_hours', '<=', $overtime_hour)
+                    //     ->all();
+                    //     if (!$overtime_multiplication) {
+                    //         $overtime_multiplication = $overtime_multiplication_all
+                    //         ->where('day_type', 'Holiday')
+                    //         ->where('day', "all")
+                    //         // ->where('from_hours', '<=', $overtime_hour)
+                    //         ->all();
+                    //     }
+                    // }else{
+                    //     $overtime_multiplication = $overtime_multiplication_all
+                    //     ->where('day_type', 'Normal')
+                    //     ->where('day', $day)
+                    //     // ->where('from_hours', '<=', $overtime_hour)
+                    //     ->all();
+                    //     if (!$overtime_multiplication){
+                    //         $overtime_multiplication = $overtime_multiplication_all
+                    //         ->where('day_type', 'Normal')
+                    //         ->where('day', 'all')
+                    //         // ->where('from_hours', '<=', $overtime_hour)
+                    //         ->all();
+                    //     }
+                    // }
+                    $remaining = $overtime_hour;
+                    if ($overtime_multiplication) {
+                        foreach ($overtime_multiplication as $multiplication) {
+                            $xrem = $Xremains->where('code', $multiplication->calculation->code)->first();
 
-            $processedMcd[] = [
-                'temp_timesheet_id' => $temptimesheet->id,
-                'no' => $item->leg_id,
-                'working_hours_id' => $customer->working_hour_id,
-                'kronos_job_number' => $item->kronos_job_number,
-                'parent_id' => $item->parent_id,
-                'oracle_job_number' => $item->oracle_job_number,
-                'employee_name' => $item->employee_name,
-                'job_dissipline' => $item->job_dissipline,
-                'leg_id' => $item->leg_id,
-                'slo_no' => $item->slo_no,
-                'date' => $item->date,
-                'basic_hours' => $working_day_hours,
-                'actual_hours' => $item->value,
-                'deduction_hours' => $deduction_hour,
-                'overtime_hours' => $overtime_hour,
-                'total_overtime_hours' => $total_overtime_hours,
-                'paid_hours' => $item->value + $total_overtime_hours - $overtime_hour,
-                'rate' => $item->rate,
-                'custom_id' => $temp_timesheet_str . '-' . $count++
-            ];
+                            // $fromHour = $multiplication->from_hours;
+                            // $toHour = $multiplication->to_hours;
+                            $to = $xrem['remaining'];
+                            $multiplier = $multiplication->calculation->multiplier;
+    
+                            if ($remaining <= 0){
+                                break;
+                            }
+                            $range = min($to, $remaining);
+                            $result = $range * $multiplier;
+                            $remaining -= $range;
+                            // update xremains
+                            $Xremains = $Xremains->map(function ($x) use ($range, $multiplication) {
+                                if ($x['code'] == $multiplication->calculation->code) {
+                                    $x['remaining'] -= $range;
+                                }
+                                return $x;
+                            });
+                            // $Xremains->whereIn('code', $multiplication->calculation->code)->first()['remaining'] -= $range;
+
+                            // return $range. ' - ' . $remaining. ' - ' . $result . '-'. $from . ' - ' . $to;
+                            $timesheet_overtime[] = [
+                                "random_string" => $temp_timesheet_str,
+                                "multiplication_id" => $multiplication->id,
+                                "multiplication_code" => $multiplication->calculation->code,
+                                "hours" => $range,
+                                "total_hours" => $result,
+                                "custom_id" => $temp_timesheet_str . '-' . $count
+                            ];
+    
+                            $total_overtime_hours += $result;
+                        }
+                    }else{
+                        $total_overtime_hours = $overtime_hour;
+                    }
+                }
+    
+                // $is_position_rate = $position_rate->firstWhere('position', $item->job_dissipline);
+                // if($item->rate > 1) {
+                //     // create new position rate
+                //     $create = [
+                //         'position' => $item->job_dissipline,
+                //         'from_date' => $temptimesheet->from_date,
+                //         'to_date' => $temptimesheet->to_date,
+                //         'type' => 'daily',
+                //         'rate' => $item->rate,
+                //         'meal_per_day' => 0
+                //     ];
+    
+                //     $create_position_rate[] = $create;
+                //     $rate = $item->rate;
+                //     // add to position rate
+                //     $position_rate->push($create);
+                // }else{
+                //     if ($is_position_rate) {
+                //         $rate = $is_position_rate['rate'];
+                //     }else{
+                //         $create = [
+                //             'position' => $item->job_dissipline,
+                //             'from_date' => $temptimesheet->from_date,
+                //             'to_date' => $temptimesheet->to_date,
+                //             'type' => 'daily',
+                //             'rate' => $item->rate,
+                //             'meal_per_day' => 0
+                //         ];
+    
+                //         $create_position_rate[] = $create;
+                //         $rate = $item->rate;
+                //         // add to position rate
+                //         $position_rate->push($create);
+                //     }
+                // }
+                // return $timesheet_overtime;
+    
+                $processedMcd[] = [
+                    'temp_timesheet_id' => $temptimesheet->id,
+                    'no' => $item->leg_id,
+                    'working_hours_id' => $customer->working_hour_id,
+                    'kronos_job_number' => $item->kronos_job_number,
+                    'parent_id' => $item->parent_id,
+                    'oracle_job_number' => $item->oracle_job_number,
+                    'employee_name' => $item->employee_name,
+                    'job_dissipline' => $item->job_dissipline,
+                    'leg_id' => $item->leg_id,
+                    'slo_no' => $item->slo_no,
+                    'date' => $item->date,
+                    'basic_hours' => $working_day_hours <= 0 ? 0 : $working_day_hours,
+                    'actual_hours' => $item->value,
+                    'deduction_hours' => $deduction_hour,
+                    'overtime_hours' => $overtime_hour,
+                    'total_overtime_hours' => $total_overtime_hours,
+                    'paid_hours' => $item->value + $total_overtime_hours - $overtime_hour,
+                    'rate' => $item->rate,
+                    'custom_id' => $temp_timesheet_str . '-' . $count++
+                ];
+
+                if($item->value > 0){
+                    $working_day_hours -= $item->value;
+                }
+            }
         }
+        
 
         $chunk = array_chunk($processedMcd, 1000);
         foreach ($chunk as $key => $value) {
